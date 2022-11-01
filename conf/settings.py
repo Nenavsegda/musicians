9e63e02349cdfe55c0dc7466a7b3c4fa02995252
Dmitry <dimachizhikov2@gmail.com>
2022-11-01 20:18:37 +0300

    Добавлен Docker

--- conf/settings.py
+++ conf/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'musicians',
+        'USER': 'root',
+        'PASSWORD': 'pass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
