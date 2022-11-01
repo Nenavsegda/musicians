f33a41a2f539215296e730a5755835e8a11ccdc5
Dmitry <dimachizhikov2@gmail.com>
2022-11-01 14:58:14 +0300

    Добавлен сериализатор

--- main/models.py
+++ main/models.py
@@ -19,7 +19,7 @@ class Album(models.Model):
 
 class Song(models.Model):
     title = models.CharField(max_length=250, verbose_name='Название песни')
-    albums = models.ManyToManyField(Album, through='AlbumSong')
+    albums = models.ManyToManyField(Album, through='AlbumSong', related_name='songs')
 
     def __str__(self):
         return self.title
@@ -31,7 +31,7 @@ class AlbumSong(models.Model):
     song_number = models.PositiveSmallIntegerField()
 
     def __str__(self):
-        return f'Песня номер {self.song_number} с альбома {self.album}'
+        return f'{self.song_number}: {self.song}'
 
     class Meta:
         unique_together = ['album', 'song_number']
--- main/serializers.py
+++ main/serializers.py
@@ -1,17 +1,30 @@
 from rest_framework import serializers
 
-from main.models import Album, Artist
+from main.models import Album, AlbumSong, Artist
 
 
 class ArtistSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Artist
-        fields = '__all__'
+        fields = ['name']
 
 
-class AlbumSerializer(serializers.ModelSerializer):
+class ListAlbumSerializer(serializers.ModelSerializer):
+    artist = serializers.StringRelatedField()
 
     class Meta:
         model = Album
-        fields = '__all__'
+        fields = ['id', 'title', 'artist', 'release_year']
+
+
+class DetailAlbumSerializer(serializers.ModelSerializer):
+    artist = serializers.StringRelatedField()
+    songs = serializers.SerializerMethodField()
+
+    def get_songs(self, obj):
+        song_order = AlbumSong.objects.filter(album=obj)
+        return [f'{song}' for song in song_order]
+
+    class Meta:
+        model = Album
+        fields = ['id', 'title', 'artist', 'release_year', 'songs']
